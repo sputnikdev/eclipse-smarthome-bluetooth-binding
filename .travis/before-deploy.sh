b043ea5eb2b03d62a568318e860419ff8a52c381
Vlad Kolotov <vkolotov@gmail.com>
2017-11-15 23:51:00 +1300

    WIP: updating GPG keys/passwords Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]
 then
-    openssl aes-256-cbc -K $encrypted_28be7055c263_key -iv $encrypted_28be7055c263_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_b5ac5f57fa2e_key -iv $encrypted_b5ac5f57fa2e_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
 fi
